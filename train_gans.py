e599bfe6a73fb873dc693e9a12584dfe301ecee7
David Liu <David@MacBook-Pro-84.home>
2020-05-23 16:01:05 -0400

    modified train_gans

--- train_gans.py
+++ train_gans.py
@@ -6,6 +6,7 @@ import torch.utils.data
 import gan
 import utils
 
+
 # all tweaking should be done in main below
 def train_gans(lst_saved_models, dataloaders, num_gans=10, num_epochs=2000):
     """
@@ -14,32 +15,30 @@ def train_gans(lst_saved_models, dataloaders, num_gans=10, num_epochs=2000):
     gans = []
     for i in range(num_gans):
         gans.append(gan.GAN(2.2, discriminator_steps=1, generator_steps=2, disc_input_dim=784,
-                gen_input_dim=100, batch_size=10, lr_disc=.0001, lr_gen=.00015))
-    
+                            gen_input_dim=100, batch_size=10, lr_disc=.0001, lr_gen=.00015))
+
     for i in range(num_gans):
         epoch = 0
-        if lst_saved_models is not None:
-            print("Loading GAN ", i ," from a previously saved model...")
+        if lst_saved_models[i] is not None:
+            print("Loading GAN ", i, " from a previously saved model...")
             (model_id, epoch) = lst_saved_models[i]
-            gans[i] = load_model(model_id, epoch)
+            gans[i] = utils.load_model(model_id, epoch)
             assert (gans[i] is not None), "Model didn't exist!"
-        
+
         if epoch < num_epochs - 1:
-            print("Training GAN ", i, "...")
-            gans[i].train(dataLoaders[i], num_epochs, epoch)
+            print("--------Training GAN ", i, "--------")
+            gans[i].train(dataloaders[i], num_epochs, epoch)
         else:
             print("GAN ", i, " was already fully trained to ", epoch, " epochs.")
-    
+
     return gans
 
 
 def main():
     num_gans = 10
-    dataloaders = utils.loadDataset(image_path='./mnist/train-images-idx3-ubyte', 
+    dataloaders = utils.loadDataset(image_path='./mnist/train-images-idx3-ubyte',
                                     label_path='./mnist/train-labels-idx1-ubyte')
-    lst_saved_models = []
-    for i in range(num_gans):
-        lst_saved_models[i] = None
+    lst_saved_models = [None for _ in range(num_gans)]
     """
     can overwrite to a saved model in this space, e.g.
     lst_saved_models[2] = (2, 1999)
